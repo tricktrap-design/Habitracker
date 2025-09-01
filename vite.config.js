bb7252eadd0abd7421e8d73bef4a81b63d330750
Emilio Arboleya <56616138+tricktrap-design@users.noreply.github.com>
2025-09-01 17:01:43 -0600

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-
 export default defineConfig({
   plugins: [react()],
-  base: '/Habitracker/'
+  base: '/Habitracker/' // must match repo name exactly (case-sensitive)
 })
-
